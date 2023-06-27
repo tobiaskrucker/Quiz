bee0cb5d4b604d0cf807ef2efc1699b01ca692dd
nihrens <136571137+nihrens@users.noreply.github.com>
2023-06-27 11:56:39 +0200

    Update passwordcompare.js
    Fehlerkorrektur

--- Quiz Anwendung/passwordcompare.js
+++ Quiz Anwendung/passwordcompare.js
@@ -2,6 +2,6 @@ function validatePassword(){
   var pass1 = document.getElementsByName("password")[0].value;
   var pass2 = document.getElementsByName("password2")[0].value;
   if (pass1 != pass2) {
-      error("Passwörter stimmen nicht überein");
+      throw new error("Passwörter stimmen nicht überein");
   } 
 }
