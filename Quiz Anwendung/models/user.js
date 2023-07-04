7cc462785b5d3dfac5646f14b956ba170f041cec
jannefunk <janne.funk@mail.de>
2023-07-04 20:55:18 +0200

    Menüband im Dashboard erstellt

--- Quiz Anwendung/models/user.js
+++ Quiz Anwendung/models/user.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
   email:String,
   username: String,
-  password: String
+  password: String,
+  role: String
 });
 
 const User = mongoose.model('User', userSchema);
