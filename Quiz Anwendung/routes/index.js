0d7398ffb856eb22b289486b60d1e8796e2189bf
Adrian <adrian.toepfer@iubh-fernstudium.de>
2023-07-27 22:17:07 +0200

    Revert "Übersicht der aktiven Spiele im Dashboard"
    This reverts commit d496eef6da2b503499577f12bdf3ea3ed2382743.

--- Quiz Anwendung/routes/index.js
+++ Quiz Anwendung/routes/index.js
@@ -111,15 +111,12 @@ router.post('/login', async (req, res) => {
 });
 
 // Dashboard-Route
-router.get('/dashboard', async ( req, res) => {
+router.get('/dashboard', (req, res) => {
   // Überprüfen, ob der Benutzer angemeldet ist
   if (!req.session.user) {
     return res.redirect('/login');
   }
-  const game = await Game.find({});
-  const module = await Module.find({});
-  res.render('dashboard.ejs', { user: req.session.user, game: game, module: module});
-  
+  res.render('dashboard.ejs', { user: req.session.user });
 });
 
 // Modeselect-Route
@@ -137,11 +134,14 @@ router.post('/selectMode', async (req, res) => {
   if (!req.session.user) {
     return res.redirect('/login');
   }
-  // Modus wird über Button ausgewählt
+  // Button bestimmt, welcher Modus ausgewählt wird und speichert in der Datenbank
   const formData = req.body;
-  req.session.setup = formData.mode;
+ // const setup = await Setup.create({mode: formData.mode});
+  
 
+  req.session.setup = formData.mode;
   // Weiterleitung zur Modulauswahl
+
   const module = await Module.find();
   res.render('modulSelect.ejs', { user: req.session.user, setup: req.session.setup, module: module });
 });
@@ -153,14 +153,13 @@ router.post('/selectModule', async (req, res) => {
     return res.redirect('/login');
   }
 
-  // Modul wird über Button ausgewählt
   const formData = req.body;
   const games = await Game.find({ modus: req.session.setup, module: formData.module })
 
   var openGame;
 
-  // Prüfen auf offene Spiele
 if(games.length > 0){
+  
   games.forEach( function(game){
 
       if (game.users.length < 2){
@@ -179,7 +178,6 @@ if(games.length > 0){
   );
 }
 
-// Anlegen eines Spiels, falls keine offenen Spiele vorhanden sind
 if(!openGame){
   var newGame = new Game();
   newGame.users[0] = req.session.user;
